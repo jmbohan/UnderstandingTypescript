f69fa3fcf6e1dcb55787841951eeef4ea8092cd6
jmbohan <jmbohan@purdue.edu>
2022-08-26 17:22:30 +0000

    generics

---
+++ generics-01-starting-setup/src/app.ts
@@ -0,0 +1,27 @@
+// const names: Array<string> = [];
+
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done');
+//     } , 2000);
+// });
+
+// promise.then(data => {
+//     // data.split(' ');
+// }) 
+
+function merge<T, U>(objA: T, objB: U){
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30});
+const mergedObj2 = merge<{name: string, hobbies: string[]}, {age: number}> ({name: 'Max', hobbies: ['Sports', 'Band', 'Cooking']}, {age: 30});
+
+
+console.log(mergedObj.age);
+console.log(mergedObj2.hobbies[1]); //This works and I don't know why there is an error
+
+
+
+
